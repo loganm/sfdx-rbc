0903f7b0150e32fb0ad98b38585b0cf0405ed4ed
Logan Moore <loganm@users.noreply.github.com>
2024-04-05 01:29:32 +1300

    Use record type instead of schema
    The schema type is meant to be used for standard salesforce objects with known schemas I think. The
    record type is more appropriate for custom objects with unknown fields. I still had to disable an
    eslint error, and I've come to terms with that.

--- src/common/salesforceObject.ts
+++ src/common/salesforceObject.ts
@@ -1,12 +1,18 @@
+import { Record } from 'jsforce';
 import { Connection } from '@salesforce/core';
-import { Schema } from 'jsforce';
 import { RbcConfig } from './config';
 import { SalesforceOrgDescribe } from './salesforceOrgDescribe';
 
 export type SalesforceObject = {
   apiName: string;
   externalIdField: string;
-  records: Schema[];
+  records: SalesforceRecord[];
+};
+
+export declare type SalesforceRecord = {
+  [field: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  externalId: string;
+  externalIdField: string;
 };
 
 export async function retrieveObject(
@@ -18,7 +24,15 @@ export async function retrieveObject(
   const recordsQuery = buildQuery(config, describe, apiName);
   const recordsQueryResult = await connection.autoFetchQuery(recordsQuery);
   const externalIdField = config.objects.find((object) => object.apiName === apiName)?.externalId as string;
-  return { apiName, externalIdField, records: recordsQueryResult.records };
+  return {
+    apiName,
+    externalIdField,
+    records: recordsQueryResult.records.map((record) => convertToSalesforceRecord(record, externalIdField)),
+  };
+}
+
+function convertToSalesforceRecord(record: Record, externalIdField: string): SalesforceRecord {
+  return { ...record, externalId: record[externalIdField] as string, externalIdField };
 }
 
 function buildQuery(config: RbcConfig, describe: SalesforceOrgDescribe, apiName: string): string {
@@ -42,10 +56,10 @@ function buildQuery(config: RbcConfig, describe: SalesforceOrgDescribe, apiName:
       }
     }
   });
-  let queryString = 'SELECT ' + fields.join(', ') + ' FROM ' + objectDescribe.name;
+  let queryString = `SELECT ${fields.join(', ')} FROM ${objectDescribe.name}`;
   if (objectConfig?.conditions) {
-    queryString += ' WHERE ' + objectConfig.conditions;
+    queryString += ` WHERE ${objectConfig.conditions}`;
   }
-  queryString += ' ORDER BY ' + objectConfig.externalId + ' ASC';
+  queryString += ` ORDER BY ${objectConfig.externalId} ASC`;
   return queryString;
 }
